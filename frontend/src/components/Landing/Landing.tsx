9fef6bedfe048f67d109757519e7d58637989b11
Elliot <elliot.winton@gmail.com>
2025-06-08 22:13:40 -0400

    quick fixes

--- frontend/src/components/Landing/Landing.tsx
+++ frontend/src/components/Landing/Landing.tsx
@@ -10,7 +10,7 @@ const Landing = () => {
     return (
         <div id='landing'>
           <div id='landing-title-text'>
-            <h1 id='landing-title'>Keep your goals consistent, no matter where you go</h1>
+            <h1 id='landing-title'>Keep your goals consistent, wherever you go</h1>
             <p id='landing-text'>Set your calories and macros to find restaurants near you that fit your needs.</p>
           </div>
           <div id='landing-search-form'>
